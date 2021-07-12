dbb40e9e625b0dde9539286042fbfe90f1d90115
Pramila Tamang <pramilatamang@bajratechnologies.com>
2021-07-12 21:59:46 +0545

    entity framework added

---
+++ DbEntities/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Application.DbEntities
+{
+    public class Student : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
---
+++ StudentMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Application
+{
+    public class StudentMap
+    {
+    }
+}
